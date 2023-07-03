0694499919486fa08454455fd4021c9a48911091
ronpeschel <ron.peschel@online.de>
2023-07-03 16:35:07 +0200

    First test Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloWorld from "./components/HelloWorld";
 
 function App() {
+
+const UserAge = 21
+const UserName = "Xavier"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld age={UserAge} name={UserName}/>
     </div>
   );
 }
